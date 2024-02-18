72563f14fd8e03dba7ac83ef1e5dfca6be044abd
Samuel Pavlík <pavlik.samuel@gmail.com>
2024-02-18 20:19:41 +0100

    ShortenUrlService with unit tests

---
+++ src/main/kotlin/com/pavlik/myurlshortener/service/impl/ShortenUrlServiceImpl.kt
@@ -0,0 +1,25 @@
+package com.pavlik.myurlshortener.service.impl
+
+import com.pavlik.myurlshortener.repository.UrlRepository
+import com.pavlik.myurlshortener.entity.UrlMapping
+import com.pavlik.myurlshortener.entity.UrlCreateRequest
+import com.pavlik.myurlshortener.service.ShortenUrlService
+import org.springframework.stereotype.Service
+
+@Service
+class ShortenUrlServiceImpl(
+    private val urlRepository: UrlRepository
+) : ShortenUrlService {
+
+    companion object {
+        var counter: Int = 0
+    }
+
+    override fun createShortUrl(request: UrlCreateRequest): UrlMapping {
+        return urlRepository.save(UrlMapping("short${counter++}", request.originalUrl))
+    }
+
+    override fun findByShortUrl(shortUrl: String): UrlMapping? {
+        return urlRepository.findByShortUrl(shortUrl)
+    }
+}
---
+++ src/test/kotlin/com/pavlik/myurlshortener/service/impl/ShortenUrlServiceImplTest.kt
@@ -0,0 +1,73 @@
+package com.pavlik.myurlshortener.service.impl
+
+import com.ninjasquad.springmockk.MockkBean
+import com.pavlik.myurlshortener.entity.UrlCreateRequest
+import com.pavlik.myurlshortener.entity.UrlMapping
+import com.pavlik.myurlshortener.repository.UrlRepository
+import com.pavlik.myurlshortener.service.ShortenUrlService
+import io.mockk.every
+import io.mockk.verify
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShortenUrlServiceImplTest {
+
+    @Autowired
+    lateinit var shortenUrlService: ShortenUrlService
+
+    @MockkBean
+    lateinit var urlRepository: UrlRepository
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun `when provided valid url then save it`() {
+        //when
+        var mapping = UrlMapping("shortUrl", "originalUrl")
+        every { urlRepository.save(any()) } answers {
+            mapping
+        }
+
+        val request = UrlCreateRequest(mapping.originalUrl)
+        val result = shortenUrlService.createShortUrl(request)
+
+        //then
+        verify { urlRepository.save(any()) }
+
+        assertEquals("shortUrl", result.shortUrl)
+        assertEquals("originalUrl", result.originalUrl)
+    }
+
+    @Test
+    fun `when provided valid url creates unique url each time`() {
+        val seenUrls = HashSet<String>()
+        val numOfReps = 100
+        val slot = mutableListOf<UrlMapping>()
+
+        //when
+        every { urlRepository.save(any()) } answers { UrlMapping() }
+
+        repeat (numOfReps) {
+            val request = UrlCreateRequest("originalUrl")
+            shortenUrlService.createShortUrl(request)
+
+            verify { urlRepository.save(capture(slot)) }
+
+            //then
+            assertFalse(seenUrls.contains(slot.last().shortUrl))
+            seenUrls.add(slot.last().shortUrl)
+        }
+    }
+}
