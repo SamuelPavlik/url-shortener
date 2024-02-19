4a54b27dc7a4c0eb39a2a1f4666e70592ff0395f
Samuel Pavlík <pavlik.samuel@gmail.com>
2024-02-19 16:55:06 +0100

    implemented simple short code generator

--- src/main/kotlin/com/pavlik/myurlshortener/service/impl/ShortenUrlServiceImpl.kt
+++ src/main/kotlin/com/pavlik/myurlshortener/service/impl/ShortenUrlServiceImpl.kt
@@ -5,18 +5,26 @@ import com.pavlik.myurlshortener.entity.UrlMapping
 import com.pavlik.myurlshortener.entity.UrlCreateRequest
 import com.pavlik.myurlshortener.service.ShortenUrlService
 import org.springframework.stereotype.Service
+import kotlin.random.Random
 
 @Service
 class ShortenUrlServiceImpl(
     private val urlRepository: UrlRepository
 ) : ShortenUrlService {
 
-    companion object {
-        var counter: Int = 0
+    override fun createShortUrl(request: UrlCreateRequest): UrlMapping {
+        val shortCode = generateSequence { generateShortCode() }
+            .first { urlRepository.findByShortCode(it) == null }
+
+        return urlRepository.save(UrlMapping(shortCode, request.originalUrl))
     }
 
-    override fun createShortUrl(request: UrlCreateRequest): UrlMapping {
-        return urlRepository.save(UrlMapping("short${counter++}", request.originalUrl))
+    private fun generateShortCode(): String {
+        val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+        return (1..8)
+            .map { Random.nextInt(0, charPool.size) }
+            .map(charPool::get)
+            .joinToString("")
     }
 
     override fun findOriginalLink(shortUrl: String): String? {
